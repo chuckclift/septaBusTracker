3daa0fc67d52c4dbfec8263c386bf4b00794fcbb
Chuck Clift <chkengr@gmail.com>
2015-07-05 13:10:32 -0400

    Brought project into git with basic functionality

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Utilize the SEPTA API to keep track of the busses you need to know about
 
   A simple program for bus tracking.  It utilizes the SEPTA API to retreive information about the busses and their routes.
+
+This code is almost completely based off of the septapy package, so go check it out at https://github.com/conorsch/septapy if you like the idea.  I didn't use the library because there were python3 compatibility issues.
---
+++ trackBus.py
@@ -0,0 +1,56 @@
+import requests
+
+class Vehicle(object):
+    
+    def __init__(self, jsonArgs, route=None):
+
+        self.latitude    = float(jsonArgs['lat'])
+        self.longitude   = float(jsonArgs['lng'])
+        self.coords      = (self.latitude, self.longitude)
+
+        self.vehicleID   = jsonArgs['VehicleID']
+        self.blockID     = jsonArgs['BlockID']
+        self.label       = jsonArgs['label']
+        self.direction   = jsonArgs['Direction']
+
+        self.title       = self.label
+        self.offset      = jsonArgs['Offset']
+        self.destination = jsonArgs['destination']
+        self.route       = route
+     
+
+def getVehiclesByRoute(routeIdentifier):        
+    vehicleURL = "http://www3.septa.org/transitview/bus_route_data/" + routeIdentifier
+    r = requests.get(vehicleURL)
+    j = r.json()
+    vehicles = j['bus']
+    return [Vehicle(v, route=routeIdentifier) for v in vehicles]
+
+
+with open("targetRoutes.txt") as t:
+    route_list = t.read()
+    route_list = route_list.split()
+    route_list = set(route_list)
+
+
+vehicle_list = [getVehiclesByRoute(r) for r in route_list]
+
+# Flattening vehicle list
+vehicle_list = [x for sublist in vehicle_list for x in sublist]
+
+with open("vehicle_numbers.txt") as v:
+    my_busses = v.read()
+    my_busses = my_busses.split()
+   
+vehicle_list = [_ for _ in vehicle_list if _.vehicleID in my_busses]
+
+
+report_string = "Bus #    Route\n"
+
+for b in vehicle_list:
+    report_string += b.vehicleID + "      " + b.route + "\n"
+
+with open("report.txt", "w") as w:
+    w.write(report_string)
+
+
